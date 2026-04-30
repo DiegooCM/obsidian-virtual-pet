f50bd6da77f2259d9e44f02e3c9bbe22ea0f9d78
DiegooCM <diegocorri2005@gmail.com>
2026-04-30 18:22:06 +0200

    Temporal solution for eslint compat

--- eslint.config.js
+++ eslint.config.js
@@ -1,34 +1,44 @@
-import { defineConfig } from "eslint/config";
-import { globalIgnores } from "eslint/config";
+import { defineConfig, globalIgnores } from "eslint/config";
+import { FlatCompat } from "@eslint/eslintrc";
+import { fileURLToPath } from "url";
+import path from "path";
 import tseslint from "typescript-eslint";
-import react from "eslint-plugin-react";
+import pluginReact from "eslint-plugin-react";
+import globals from "globals";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const compat = new FlatCompat({ baseDirectory: __dirname });
 
 export default defineConfig([
+  ...compat.plugins("eslint-plugin-obsidianmd"),
+  pluginReact.configs.flat.recommended,
+  ...tseslint.configs.recommended,
   {
-    files: ["**/*.ts", "**/*.tsx"],
+    files: ["**/*.{js,ts,jsx,tsx}"],
     languageOptions: {
+      globals: globals.browser,
       parser: tseslint.parser,
       parserOptions: {
-        ecmaFeatures: {
-          jsx: true,
-        },
+        ecmaFeatures: { jsx: true },
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
       },
     },
     plugins: {
       "@typescript-eslint": tseslint.plugin,
-      react,
+      react: pluginReact,
     },
     rules: {
-      ...react.configs.recommended.rules,
-      ...tseslint.configs.recommended.rules,
       semi: "error",
       "prefer-const": "error",
       "react/react-in-jsx-scope": "off",
+      "obsidianmd/sample-names": "off",
+      "react/display-name": "off",
     },
     settings: {
-      react: {
-        version: "detect",
-      },
+      react: { version: "detect" },
     },
   },
   globalIgnores(["node_modules/", "main.js"]),
